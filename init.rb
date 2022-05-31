e878f1a48ad2ecc52d46ffb04619b6e3bcea6023
Yuji Komatsu <y.komatsu.xe@future.co.jp>
2022-05-31 21:28:07 +0900

    up to 0.2.1

--- init.rb
+++ init.rb
@@ -3,9 +3,9 @@ require 'gitlab_repositories_helper_patch'
 
 Redmine::Plugin.register :redmine_gitlab_adapter do
   name 'Redmine Gitlab Adapter plugin'
-  author 'Komatsu Yuji'
+  author 'Future Corporation'
   description 'This is a Gitlab Adapter plugin for Redmine'
-  version '0.2.0'
+  version '0.2.1'
   url 'https://www.future.co.jp'
   author_url 'https://www.future.co.jp'
   Redmine::Scm::Base.add "Gitlab"
